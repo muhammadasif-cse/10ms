71fb7bb6546bef00678a88322defd3d485a3da90
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-07-25 19:09:10 +0600

    feat: replace Main component with Instructor for better instructor section handling

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import Hero from "./components/hero/hero";
 import { TProduct } from "../types/product";
 import { fetchAPI } from "@/lib/fetch-api";
 import { Locale } from "next-intl";
-import Main from "./components/sections/main/main";
+import Instructor from "./components/sections/instructor";
 
 type Props = {
   params: Promise<{ locale: Locale }>;
@@ -13,11 +13,26 @@ export default async function IndexPage({ params }: Props) {
   if (!products || Object.keys(products).length === 0) {
     return <div className="text-center p-4">No products available</div>;
   }
+  if (!products.sections || products.sections.length === 0) {
+    return <div className="text-center p-4">No sections available</div>;
+  }
+  const instructorSection = products.sections.find(
+    (section) => section.type === "instructors"
+  );
+
+  if (!instructorSection || !instructorSection.values?.length) return null;
 
   return (
     <div>
       <Hero data={products} />
-      <Main data={products} />
+      <div className="flex-1 w-full mx-auto container">
+        <div className=" md:max-w-[calc(100%_-_348px)] lg:max-w-[calc(100%_-_448px)]">
+          <Instructor
+            title={instructorSection.name || ""}
+            data={instructorSection.values}
+          />
+        </div>
+      </div>
     </div>
   );
 }
